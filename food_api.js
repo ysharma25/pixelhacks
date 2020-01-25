f87d060056723eec3fccf2608692aa5e16a0bc9f
ysharma25 <52763286+ysharma25@users.noreply.github.com>
2020-01-25 13:07:24 -0800

    Update food_api.js

--- food_api.js
+++ food_api.js
@@ -3,7 +3,7 @@ var request;
 
 function askFood() {
   var apiId = "50542467";
-  var apiKey = "f3be5bfef9eae9ad0f8e62ab01d504b1	—";
+  var apiKey = "f3be5bfef9eae9ad0f8e62ab01d504b1";
   var foodname = document.getElementById("foodname").value;
   if (foodname === "") {
     alert('You did not enter an ingredient!');
@@ -13,20 +13,20 @@ function askFood() {
   console.log("Name of recipe: " + foodname);
 
   //var query = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&location=" + foodname;
-  var query = "https://api.edamam.com/search?q=chicken&app_id=" + apiId+ "&app_key="+ apiKey+"&from=0&to=3&calories=591-722&health=alcohol-free";
+  var query = "https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q="+ foodname+"&app_id=" + apiId+ "&app_key="+ apiKey;
   query = query.replace(/ /g, "-")
   foodnameRequest = new XMLHttpRequest();
   foodnameRequest.open("GET", query, true);
   foodnameRequest.setRequestHeader('Authorization', apiKey);
-  foodnameRequest.onload = findLocation;
+  foodnameRequest.onload = findFood;
   foodnameRequest.send();
 }
 
-function findLocation(){
+function findFood(){
   if(foodnameRequest.status != 200){
     alert("Error");
     return;
   }
-  var foodInfo = JSON.parse(foodnameRequest.responseText);
+  var foodInfo = foodnameRequest;
   console.log(foodInfo);
 }
